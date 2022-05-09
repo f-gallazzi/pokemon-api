15fff195eb5b46973a70b1757ef9a88510740c03
Francisco <fgallazzi@lagom.us>
2022-05-09 12:26:40 -0300

    allows all origins

--- routes/routes.go
+++ routes/routes.go
@@ -3,12 +3,15 @@ package routes
 import (
 	"pokemons-challenge/services"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Setup() *gin.Engine {
 
 	r := gin.Default()
+
+	r.Use(cors.Default())
 
 	services.SetupRoutesPokemon(r.Group("/pokemons"))
 	services.SetupRoutesImage(r.Group("/images"))
