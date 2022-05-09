b4963f3b2bc5f1a7ea29bb8160146c54bcdb9096
Francisco <fgallazzi@lagom.us>
2022-05-09 12:41:55 -0300

    set middleware cors

--- routes/routes.go
+++ routes/routes.go
@@ -3,7 +3,6 @@ package routes
 import (
 	"pokemons-challenge/services"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,10 +10,26 @@ func Setup() *gin.Engine {
 
 	r := gin.Default()
 
-	r.Use(cors.Default())
+	r.Use(CORSMiddleware())
 
 	services.SetupRoutesPokemon(r.Group("/pokemons"))
 	services.SetupRoutesImage(r.Group("/images"))
 
 	return r
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
